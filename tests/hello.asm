378bbd78ac0a3231256ef8cce8a0489074f40f6e
Reuben Thomas <rrt@sc3d.org>
2019-01-06 23:53:12 +0000

    tests: add machinery for running SMite shell script tests
    Use this for the hello, world test.
    
    Rename hello.txt to hello.asm. This works nicely for editing with Emacs.

--- tests/hello.asm
+++ tests/hello.asm
@@ -1,3 +1,13 @@
+// Hello world demo/test.
+//
+// (c) Reuben Thomas 2018
+//
+// The package is distributed under the GNU Public License version 3, or,
+// at your option, any later version.
+//
+// THIS PROGRAM IS PROVIDED AS IS, WITH NO WARRANTY. USE IS AT THE USER‘S
+// RISK.
+
 // 0
 number 10
 number 14
@@ -32,4 +42,4 @@ byte 0x64
 byte 0x21
 byte 0x0a
 
-save hello.obj
+run
