bd0465fb3790d2d54ccc81673f5ddf87d4c30495
Alistair Turnbull <github@minworks.co.uk>
2019-02-02 06:09:28 +0000

    Move stuff from "gen-actions" to "language.py" and rewrite the ugly parts.

--- src/language.py
+++ src/language.py
@@ -46,22 +46,22 @@ state_probabilities = [
     for state, transitions in enumerate(predictor)
 ]
 
-# TODO: This should be a dict, because the paths are distinct.
-language = [] # [(tuple of events (str), estimated_count (int))]
+# tuple of events (str) -> estimated_count (float)
+language = {}
 
 def walk(path, distribution):
     '''
      - path - list of event (bytes).
      - distribution - list of estimated frequency (float).
     '''
-    # Check for repetition.
-    for n in range(1, len(path)//2):
-        if path[-n:]==path[-2*n:-n]: return
     # Check for statistical irrelevance (but keep singleton paths).
     estimated_count = sum(distribution)
     if len(path) > 1 and estimated_count < 10000.: return
     # Okay, we'll keep it.
-    language.append((tuple(path), estimated_count))
+    language[tuple(path)] = estimated_count
+    # Check for repetition.
+    for n in range(1, len(path)//2):
+        if path[-n:]==path[-2*n:-n]: return
     # For all (state, action) pairs, propagate count to the successor state.
     successors = {event.name: [0.] * len(predictor) for event in ALL_EVENTS}
     for state, estimated_count in enumerate(distribution):
@@ -78,10 +78,122 @@ def walk(path, distribution):
 
 walk([], [float(x) for x in state_counts])
 
+# Sanity checks.
+
+for event in ALL_EVENTS:
+    assert (event.name,) in language
+
+for path in language:
+    assert path[1:] in language
+    assert path[:-1] in language
+
+# For each path, sort the possible next events by probability (decreasing).
+
+print("Choosing guesses")
+
+def successor(path, event):
+    # Appends one event to `path` then removes the repeated suffix, if any.
+    path += (event.name,)
+    for n in range(1, len(path)//2 + 1):
+        if path[-n:]==path[-2*n:-n]: return path[:-n]
+    return path
+
+# path -> [Event]
+path_guesses = {
+    path: sorted(
+        [
+            event
+            for event in ALL_EVENTS
+            if path + (event.name,) in language
+        ],
+        key=lambda e: language[successor(path, e)],
+        reverse=True,
+    )
+    for path in language
+}
+
+# Generate state space of (path × rejects) with flood fill.
+
+print("Generating control flow graph")
+
+states = [] # List of (path, rejects)
+state_index = {} # Inverse of `states`.
+
+# For each state in `states`, there is a transition in `transitions`.
+# It can be:
+#  - None - return;
+#  - int g - goto g;
+#  - (Event e, int c, int w) - if (next==e) { e; goto c; } else goto w;
+transitions = []
+
+def enqueue(path, rejects):
+    '''
+    Adds a state if it has not been seen before.
+     - path - tuple of event name.
+     - rejects - frozenset of event name.
+    '''
+    state = (path, rejects)
+    if state not in state_index:
+        state_index[state] = len(state_index)
+        states.append(state)
+    return state_index[state]
+
+assert enqueue((), frozenset()) == 0 # Root state.
+
+# The work queue is the suffix of `states` that does not yet have transitions.
+while len(transitions) < len(states):
+    path, rejects = states[len(transitions)]
+    for guess in path_guesses[path]:
+        if guess.name not in rejects:
+            # We have a guess.
+            transitions.append((
+                guess,
+                enqueue(successor(path, guess), frozenset()),
+                enqueue(path, rejects.union({guess.name})),
+            ))
+            break
+    else:
+        # There is no good guess.
+        new_path = path[1:]
+        if new_path:
+            transitions.append(enqueue(new_path, rejects))
+        else:
+            transitions.append(None)
+
+assert len(states) == len(state_index) == len(transitions)
+
+# Tension branches.
+
+print("Tensioning branches")
+
+# We only need labels for the states in which we make a guess.
+label_map = {} # state -> label
+for state, transition in enumerate(transitions):
+    if isinstance(transition, tuple):
+        label_map[state] = len(label_map)
+
+def short_circuit(state):
+    # Follows gotos, and maps `state` to a label or `None`.
+    while isinstance(transitions[state], int):
+        state = transitions[state]
+    return label_map.get(state)
+
+# List of (guess, label or None, label or None).
+labels = [
+    (guess.name, short_circuit(c), short_circuit(w))
+    for guess, c, w in [
+        t
+        for t in transitions
+        if isinstance(t, tuple)
+    ]
+]
+
+assert len(label_map) == len(labels)
+
 # Dump to a file.
 
-out_filename = '/tmp/language.pickle'
+out_filename = '/tmp/labels.pickle'
 with open(out_filename, 'wb') as f:
-    # [((str, ...), float)]
-    pickle.dump(language, file=f)
+    # [(event_name (str), if_correct (int?), if_wrong (int?))]
+    pickle.dump(labels, file=f)
 print('Wrote {}'.format(out_filename))
