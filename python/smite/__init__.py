928a6baf36edd7dc2459ff1bd5e10397e3faecf8
Reuben Thomas <rrt@sc3d.org>
2019-02-22 19:34:45 +0000

    Python: update step “no error” exit code
    Was -257, but is now supposed to be -128.

--- python/smite/__init__.py
+++ python/smite/__init__.py
@@ -188,7 +188,7 @@ class State:
             return self.step(addr=self.registers["MEMORY"].get() + 1, trace=True)
         else:
             ret = libsmite.smite_run(self.state)
-            if ret != -257:
+            if ret != -128:
                 print("HALT code {} was returned".format(ret));
             return ret
 
@@ -199,10 +199,10 @@ class State:
         while True:
             ret = libsmite.smite_single_step(self.state)
             done += 1
-            if ret != -257 or self.registers["PC"].get() == addr or (addr == None and done == n):
+            if ret != -128 or self.registers["PC"].get() == addr or (addr == None and done == n):
                 break
 
-        if ret != -257:
+        if ret != -128:
             if ret != 0:
                 print("HALT code {} was returned".format(ret), end='')
                 if n > 1:
