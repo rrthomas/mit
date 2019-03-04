096425a386385498e1895b1aeb358d97f30a8bf7
Reuben Thomas <rrt@sc3d.org>
2019-03-04 18:50:22 +0000

    action_gen.py: further clarification of the stack operation generation code
    See issue #71. Thanks, @apt1002!

--- src/smite_core/action_gen.py
+++ src/smite_core/action_gen.py
@@ -33,11 +33,11 @@ class Action:
 COUNT_NAME = 'COUNT'
 VARIADIC_NAME = 'ITEMS'
 
-def stack_item_has_var(item):
+def is_named_stack_item(item):
     return item != VARIADIC_NAME
 
 def _stack_item_to_var(item):
-    assert stack_item_has_var(item)
+    assert is_named_stack_item(item)
     return item
 
 def stack_item_name(item):
@@ -49,7 +49,7 @@ def stack_item_type(item):
 
 # Calculate (symbolic) depth of stack picture
 def item_size(item):
-    if stack_item_has_var(item):
+    if is_named_stack_item(item):
         return '(sizeof({}) / smite_word_size)'.format(stack_item_type(item))
     else:
         return '(smite_UWORD)COUNT'
@@ -59,9 +59,9 @@ def stack_depth(stack):
     return '({})'.format(depth if depth != '' else '0')
 
 # Variable creator
-def make_vars(stack):
+def declare_vars(stack):
     return '\n'.join(['{} {};'.format(stack_item_type(v), stack_item_name(v))
-                      for v in stack if stack_item_has_var(v)])
+                      for v in stack if is_named_stack_item(v)])
 
 def disable_warnings(warnings, c_source):
     '''
@@ -76,20 +76,27 @@ def disable_warnings(warnings, c_source):
                                      pragmas='\n'.join(['#pragma GCC diagnostic ignored "{}"'.format(w)
                                                         for w in warnings]))
 
-def check_static_args(args):
-    return disable_warnings(['-Wtype-limits', '-Wunused-variable'], '''\
-if ((S->STACK_DEPTH < {nargs})) {{
-    S->BAD_ADDRESS = {nargs};
-    const smite_UWORD args = 0;
+def check_pops(num_pops):
+    '''
+    Returns C source code to check that the stack contains enough items to
+    pop the specified number of items.
+     - num_pops - a C expression giving the number of items to pop.
+    Assumes:
+     - No items have been popped so far. This is needed in the error case.
+    '''
+    return disable_warnings(['-Wtype-limits', '-Wunused-variable', '-Wshadow'], '''\
+if ((S->STACK_DEPTH < {num_pops})) {{
+    S->BAD_ADDRESS = {num_pops};
+    const smite_UWORD static_args = 0;
     RAISE(3);
-}}'''.format(nargs=len([arg for arg in args if stack_item_has_var(arg)])))
+}}'''.format(num_pops=num_pops))
 
-def check_dynamic_args_and_results(args, results):
-    return disable_warnings(['-Wtype-limits'], '''\
-if ((S->STACK_DEPTH < {nargs}) || (S->STACK_SIZE - (S->STACK_DEPTH - {nargs}) < {nresults})) {{
-    S->BAD_ADDRESS = {nresults} - {nargs};
+def check_pops_then_pushes(num_pops, num_pushes):
+    return disable_warnings(['-Wtype-limits', '-Wtautological-compare'], '''\
+if ({num_pushes} > {num_pops} && (S->STACK_SIZE - S->STACK_DEPTH < {num_pushes} - {num_pops})) {{
+    S->BAD_ADDRESS = {num_pushes} - {num_pops};
     RAISE(2);
-}}'''.format(nargs=stack_depth(args), nresults=stack_depth(results)))
+}}'''.format(num_pops=num_pops, num_pushes=num_pushes))
 
 def load_var(pos, var):
     if stack_item_type(var) != 'smite_WORD':
@@ -102,16 +109,25 @@ def load_var(pos, var):
         type=stack_item_type(var)) + ';'
 
 def load_args(args):
+    '''
+    Returns C source code to read the named arguments from the stack into C
+    variables.
+    `S->STACK_DEPTH` is not modified.
+    '''
     code = []
     pos = ['-1']
     for arg in reversed(args):
         pos.append(str(item_size(arg)))
-        if stack_item_has_var(arg):
+        if is_named_stack_item(arg):
             code.append(load_var("+".join(pos), arg))
-    code.append('const smite_UWORD args = {};'.format(len([arg for arg in args if stack_item_has_var(arg)])))
     return '\n'.join(code)
 
 def store_var(pos, var):
+    '''
+    Returns C source code to write the named arguments from C variables into
+    the stack.
+    `S->STACK_DEPTH` must be modified first.
+    '''
     if stack_item_type(var) != 'smite_WORD':
         fmt = 'UNCHECKED_STORE_STACK_TYPE({pos}, {type}, {var})'
     else:
@@ -126,7 +142,7 @@ def store_results(args, results):
     pos = ['-1']
     for result in reversed(results):
         pos.append(str(item_size(result)))
-        if stack_item_has_var(result):
+        if is_named_stack_item(result):
             code.append(store_var("+".join(pos), result))
     return '\n'.join(code)
 
@@ -139,14 +155,16 @@ def dispatch(actions, prefix, undefined_case):
     for (instruction, action) in actions.__members__.items():
         # Concatenate the pieces.
         code = '\n'.join([
-            make_vars(action.value.args),
-            make_vars(action.value.results),
-            check_static_args(action.value.args),
+            declare_vars(action.value.args),
+            declare_vars(action.value.results),
+            'const smite_UWORD static_args = {};'.format(stack_depth([arg for arg in action.value.args if is_named_stack_item(arg)])),
+            check_pops('static_args'),
             load_args(action.value.args),
-            check_dynamic_args_and_results(action.value.args, action.value.results),
-            'S->STACK_DEPTH -= args;',
+            check_pops(stack_depth(action.value.args)),
+            check_pops_then_pushes(stack_depth(action.value.args), stack_depth(action.value.results)),
+            'S->STACK_DEPTH -= static_args;',
             textwrap.dedent(action.value.code.rstrip()),
-            'S->STACK_DEPTH += {nresults} - ({nargs} - {nstatic_args});'.format(nargs=stack_depth(action.value.args), nresults=stack_depth(action.value.results), nstatic_args=len([arg for arg in action.value.args if stack_item_has_var(arg)])),
+            'S->STACK_DEPTH += {num_pushes} - ({num_pops} - {nstatic_args});'.format(num_pops=stack_depth(action.value.args), num_pushes=stack_depth(action.value.results), nstatic_args='static_args'),
             store_results(action.value.args, action.value.results),
         ])
         # Remove newlines resulting from empty strings in the above.
