f4e5968479433402451fe015068fdc309cd45690
Reuben Thomas <rrt@sc3d.org>
2019-03-30 09:49:51 +0000

    Reorder instructions so CALL, BRANCH, BRANCHZ have low opcodes (fix 92)

--- src/smite_core/vm_data.py
+++ src/smite_core/vm_data.py
@@ -36,15 +36,33 @@ class Instructions(Enum):
         NEXT;'''
     )
 
-    POP = Instruction(0x01, ['item'], [], '''\
+    BRANCH = Instruction(0x01, ['addr'], [], '''\
+        S->PC = (smite_UWORD)addr;
+        NEXT;
+    ''')
+
+    BRANCHZ = Instruction(0x02, ['flag', 'addr'], [], '''\
+        if (flag == 0) {
+            S->PC = (smite_UWORD)addr;
+            NEXT;
+        }
+    ''')
+
+    CALL = Instruction(0x03, ['addr'], ['ret_addr'], '''\
+        ret_addr = S->PC;
+        S->PC = (smite_UWORD)addr;
+        NEXT;
+    ''')
+
+    POP = Instruction(0x04, ['item'], [], '''\
         (void)item;
     ''')
 
-    DUP = Instruction(0x02, ['ITEMS', 'COUNT'], ['ITEMS', 'dupee'], '''\
+    DUP = Instruction(0x05, ['ITEMS', 'COUNT'], ['ITEMS', 'dupee'], '''\
         dupee = *UNCHECKED_STACK(COUNT);
     ''')
 
-    SWAP = Instruction(0x03, ['ITEMS', 'COUNT'], ['ITEMS'], '''\
+    SWAP = Instruction(0x06, ['ITEMS', 'COUNT'], ['ITEMS'], '''\
         if (COUNT > 0) {
             smite_WORD swapee = *UNCHECKED_STACK(COUNT);
             smite_WORD top = *UNCHECKED_STACK(0);
@@ -53,14 +71,14 @@ class Instructions(Enum):
         }
     ''')
 
-    LIT = Instruction(0x04, [], ['n'], '''\
+    LIT = Instruction(0x07, [], ['n'], '''\
         int ret = load_word(S, S->PC, &n);
         if (ret != 0)
             RAISE(ret);
         S->PC += WORD_SIZE;
     ''')
 
-    LIT_PC_REL = Instruction(0x05, [], ['n'], '''\
+    LIT_PC_REL = Instruction(0x08, [], ['n'], '''\
         int ret = load_word(S, S->PC, &n);
         if (ret != 0)
             RAISE(ret);
@@ -68,85 +86,85 @@ class Instructions(Enum):
         S->PC += WORD_SIZE;
     ''')
 
-    NOT = Instruction(0x06, ['x'], ['r'], '''\
+    NOT = Instruction(0x09, ['x'], ['r'], '''\
         r = ~x;
     ''')
 
-    AND = Instruction(0x07, ['x', 'y'], ['r'], '''\
+    AND = Instruction(0x0a, ['x', 'y'], ['r'], '''\
         r = x & y;
     ''')
 
-    OR = Instruction(0x08, ['x', 'y'], ['r'], '''\
+    OR = Instruction(0x0b, ['x', 'y'], ['r'], '''\
         r = x | y;
     ''')
 
-    XOR = Instruction(0x09, ['x', 'y'], ['r'], '''\
+    XOR = Instruction(0x0c, ['x', 'y'], ['r'], '''\
         r = x ^ y;
     ''')
 
-    LSHIFT = Instruction(0x0a, ['x', 'n'], ['r'], '''\
+    LSHIFT = Instruction(0x0d, ['x', 'n'], ['r'], '''\
         r = n < (smite_WORD)smite_word_bit ? x << n : 0;
     ''')
 
-    RSHIFT = Instruction(0x0b, ['x', 'n'], ['r'], '''\
+    RSHIFT = Instruction(0x0e, ['x', 'n'], ['r'], '''\
         r = n < (smite_WORD)smite_word_bit ? (smite_WORD)((smite_UWORD)x >> n) : 0;
     ''')
 
-    ARSHIFT = Instruction(0x0c, ['x', 'n'], ['r'], '''\
+    ARSHIFT = Instruction(0x0f, ['x', 'n'], ['r'], '''\
         r = ARSHIFT(x, n);
     ''')
 
-    EQ = Instruction(0x0d, ['a', 'b'], ['flag'], '''\
+    EQ = Instruction(0x10, ['a', 'b'], ['flag'], '''\
         flag = a == b;
     ''')
 
-    LT = Instruction(0x0e, ['a', 'b'], ['flag'], '''\
+    LT = Instruction(0x11, ['a', 'b'], ['flag'], '''\
         flag = a < b;
     ''')
 
-    ULT = Instruction(0x0f, ['a', 'b'], ['flag'], '''\
+    ULT = Instruction(0x12, ['a', 'b'], ['flag'], '''\
         flag = (smite_UWORD)a < (smite_UWORD)b;
     ''')
 
-    NEGATE = Instruction(0x10, ['a'], ['r'], '''\
+    NEGATE = Instruction(0x13, ['a'], ['r'], '''\
         r = -a;
     ''')
 
-    ADD = Instruction(0x11, ['a', 'b'], ['r'], '''\
+    ADD = Instruction(0x14, ['a', 'b'], ['r'], '''\
         r = a + b;
     ''')
 
-    MUL = Instruction(0x12, ['a', 'b'], ['r'], '''\
+    MUL = Instruction(0x15, ['a', 'b'], ['r'], '''\
         r = a * b;
     ''')
 
-    DIVMOD = Instruction(0x13, ['a', 'b'], ['q', 'r'], '''\
+    DIVMOD = Instruction(0x16, ['a', 'b'], ['q', 'r'], '''\
         if (b == 0)
           RAISE(SMITE_ERR_DIVISION_BY_ZERO);
         q = a / b;
         r = a % b;
     ''')
 
-    UDIVMOD = Instruction(0x14, ['a', 'b'], ['q', 'r'], '''\
+    UDIVMOD = Instruction(0x17, ['a', 'b'], ['q', 'r'], '''\
         if (b == 0)
           RAISE(SMITE_ERR_DIVISION_BY_ZERO);
         q = (smite_WORD)((smite_UWORD)a / (smite_UWORD)b);
         r = (smite_WORD)((smite_UWORD)a % (smite_UWORD)b);
     ''')
 
-    LOAD = Instruction(0x15, ['addr'], ['x'], '''\
+    LOAD = Instruction(0x18, ['addr'], ['x'], '''\
         int ret = load_word(S, addr, &x);
         if (ret != 0)
             RAISE(ret);
     ''')
 
-    STORE = Instruction(0x16, ['x', 'addr'], [], '''\
+    STORE = Instruction(0x19, ['x', 'addr'], [], '''\
         int ret = store_word(S, addr, x);
         if (ret != 0)
             RAISE(ret);
     ''')
 
-    LOADB = Instruction(0x17, ['addr'], ['x'], '''\
+    LOADB = Instruction(0x1a, ['addr'], ['x'], '''\
         smite_BYTE b_;
         int ret = load_byte(S, addr, &b_);
         if (ret != 0)
@@ -154,28 +172,10 @@ class Instructions(Enum):
         x = (smite_WORD)b_;
     ''')
 
-    STOREB = Instruction(0x18, ['x', 'addr'], [], '''\
+    STOREB = Instruction(0x1b, ['x', 'addr'], [], '''\
         int ret = store_byte(S, addr, (smite_BYTE)x);
         if (ret != 0)
             RAISE(ret);
-    ''')
-
-    BRANCH = Instruction(0x19, ['addr'], [], '''\
-        S->PC = (smite_UWORD)addr;
-        NEXT;
-    ''')
-
-    BRANCHZ = Instruction(0x1a, ['flag', 'addr'], [], '''\
-        if (flag == 0) {
-            S->PC = (smite_UWORD)addr;
-            NEXT;
-        }
-    ''')
-
-    CALL = Instruction(0x1b, ['addr'], ['ret_addr'], '''\
-        ret_addr = S->PC;
-        S->PC = (smite_UWORD)addr;
-        NEXT;
     ''')
 
     GET_WORD_SIZE = Instruction(0x1c, [], ['r'], '''\
