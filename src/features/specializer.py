7fa5e1251ccc671a16dacde07c4608dfc638cf5e
Alistair Turnbull <github@minworks.co.uk>
2019-05-22 23:10:07 +0100

    gen-specializer: Specialize variadic instructions for `tos_constant` (fix #126)

--- src/features/specializer.py
+++ src/features/specializer.py
@@ -10,7 +10,45 @@ RISK.
 '''
 
 from mit_core.code_buffer import Code
+from mit_core.vm_data import Instruction
+from mit_core.instruction import AbstractInstruction
 from mit_core.instruction_gen import Size, StackItem, StackPicture, StackEffect
+
+
+def _replace_items(picture, replacement):
+    '''
+    Replaces 'ITEMS' with `replacement` in `picture`
+    '''
+    ret = []
+    for item in picture:
+        if item == 'ITEMS':
+            ret.extend(replacement)
+        else:
+            ret.append(item)
+    return ret
+
+def _gen_specialized_instruction(instruction, tos_constant):
+    replacement = ['x{}'.format(i) for i in range(tos_constant)]
+    code = Code()
+    code.append('assert(COUNT == {});'.format(tos_constant))
+    code.extend(instruction.code)
+    return (
+        instruction.opcode,
+        _replace_items(instruction.args, replacement),
+        _replace_items(instruction.results, replacement),
+        code,
+        instruction.terminal,
+    )
+
+SpecializedInstruction = AbstractInstruction('SpecializedInstruction', {
+    '{name}_WITH_{tos_constant}'.format(
+        name=instruction.name,
+        tos_constant=tos_constant,
+    ): _gen_specialized_instruction(instruction, tos_constant)
+    for instruction in Instruction
+    if 'ITEMS' in instruction.args
+    for tos_constant in range(4)
+})
 
 
 class CacheState:
