38330845e4f47afa54a1b611b22b342a5bfe2b80
Reuben Thomas <rrt@sc3d.org>
2019-08-10 23:28:07 +0200

    IPython: suppress Python tracebacks, so we just get errors

---
+++ python/mit/ipython_suppress_traceback.py
@@ -0,0 +1,14 @@
+# IPython extension: suppress traceback
+# From https://stackoverflow.com/questions/46222753/how-do-i-suppress-tracebacks-in-jupyter
+
+import sys
+
+def hide_traceback(exc_tuple=None, filename=None, tb_offset=None,
+                   exception_only=False, running_compiled_code=False):
+    etype, value, tb = sys.exc_info()
+    return _ipython._showtraceback(etype, value, _ipython.InteractiveTB.get_exception_only(etype, value))
+
+def load_ipython_extension(ipython):
+    global _ipython
+    _ipython = ipython
+    ipython.showtraceback = hide_traceback
