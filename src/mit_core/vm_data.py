1b68d9f5625539501ae6d73051544673be44235d
Reuben Thomas <rrt@sc3d.org>
2019-09-26 11:53:59 +0100

    vm_data.py: convert spec to YAML (fix #52)

--- src/mit_core/vm_data.py
+++ src/mit_core/vm_data.py
@@ -7,190 +7,157 @@
 # THIS PROGRAM IS PROVIDED AS IS, WITH NO WARRANTY. USE IS AT THE USER’S
 # RISK.
 
+import os
 from enum import Enum, IntEnum, unique
 
+import yaml
+
+from .autonumber import AutoNumber
 from .code_util import Code
 from .instruction import InstructionEnum
-from .register import RegisterEnum
 
 
-@unique
-class Register(RegisterEnum):
-    '''VM registers.'''
-    pc = ()
-    ir = ()
-    bad = ()
-    stack_depth = ()
+with open(os.path.join(os.path.dirname(__file__), 'mit_spec.yaml')) as f:
+    spec = yaml.load(f.read())
+
+
+class RegisterEnum(AutoNumber):
+    def __init__(self, read_only=False, type='mit_uword'):
+        self.read_only = read_only
+        self.type = type
+
+register = {r: () for r in spec['Register']}
+register.update({
     # Registers that are not part of the spec
-    memory = (True, 'mit_word *')
-    stack = (True, 'mit_word * restrict')
-    memory_bytes = (True,)
-    stack_words = (True,)
+    'memory': (True, 'mit_word *'),
+    'stack': (True, 'mit_word * restrict'),
+    'memory_bytes': (True,),
+    'stack_words': (True,),
+})
+Register = unique(RegisterEnum('Register', register))
+Register.__doc__ = 'VM registers.'
 
-@unique
-class MitErrorCode(IntEnum):
-    'VM error codes returned by e.g. `mit_run()`.'
-    OK = 0
-    INVALID_OPCODE = 1
-    STACK_OVERFLOW = 2
-    INVALID_STACK_READ = 3
-    INVALID_STACK_WRITE = 4
-    INVALID_MEMORY_READ = 5
-    INVALID_MEMORY_WRITE = 6
-    UNALIGNED_ADDRESS = 7
-    BAD_SIZE = 8
-    DIVISION_BY_ZERO = 9
-    HALT = 127
+MitErrorCode = unique(IntEnum('MitErrorCode', spec['ErrorCode']))
 
-@unique
-class Instruction(InstructionEnum):
-    '''VM instruction opcodes.'''
-    NEXT = (0x0, [], [], Code('''\
-        DO_NEXT;'''
-    ), True)
+def instruction_enum(enum_name, docstring, spec, code):
+    enum = unique(InstructionEnum(
+        enum_name,
+        ((
+            name,
+            (i['opcode'], i['args'], i['results'], code[name],
+             i.get('terminal', False)),
+        ) for name, i in spec.items())
+    ))
+    enum.__doc__ = docstring
+    return enum
 
-    JUMP = (0x1, ['addr'], [], Code('''\
-        S->pc = (mit_uword)addr;
-        CHECK_ALIGNED(S->pc);
-        DO_NEXT;'''
-    ), True)
+Instruction = instruction_enum(
+    'Instruction',
+    'VM instruction opcodes.',
+    spec['Instruction'],
+    {
+        'NEXT': Code('DO_NEXT;'),
 
-    JUMPZ = (0x2, ['flag', 'addr'], [], Code('''\
-        if (flag == 0) {
+        'JUMP': Code('''\
             S->pc = (mit_uword)addr;
             CHECK_ALIGNED(S->pc);
-            DO_NEXT;
-        }
-    '''))
+            DO_NEXT;'''
+        ),
 
-    CALL = (0x3, ['addr'], ['ret_addr'], Code('''\
-        ret_addr = S->pc;
-        S->pc = (mit_uword)addr;
-        CHECK_ALIGNED(S->pc);
-        DO_NEXT;'''
-    ), True)
+        'JUMPZ': Code('''\
+            if (flag == 0) {
+                S->pc = (mit_uword)addr;
+                CHECK_ALIGNED(S->pc);
+                DO_NEXT;
+            }
+        '''),
 
-    POP = (0x4, ['ITEMS', 'COUNT'], [], Code())
+        'CALL': Code('''\
+            ret_addr = S->pc;
+            S->pc = (mit_uword)addr;
+            CHECK_ALIGNED(S->pc);
+            DO_NEXT;'''
+        ),
 
-    DUP = (0x5, ['x', 'ITEMS', 'COUNT'], ['x', 'ITEMS', 'x'], Code())
+        'POP': Code(),
+            'DUP': Code(),
+            'SWAP': Code(),
 
-    SWAP = (0x6, ['x', 'ITEMS', 'y', 'COUNT'], ['y', 'ITEMS', 'x'], Code())
+        'PUSH_STACK_DEPTH': Code('n = S->stack_depth;'),
 
-    PUSH_STACK_DEPTH = (0x7, [], ['n'], Code('''\
-        n = S->stack_depth;'''
-    ))
+        'LOAD': Code('''\
+            int ret = load(S->memory, S->memory_bytes, addr, size, &x);
+            if (ret != 0) {
+                S->bad = addr;
+                RAISE(ret);
+            }'''
+        ),
 
-    LOAD = (0x8, ['addr', 'size'], ['x'], Code('''\
-        int ret = load(S->memory, S->memory_bytes, addr, size, &x);
-        if (ret != 0) {
-            S->bad = addr;
-            RAISE(ret);
-        }'''
-    ))
+        'STORE': Code('''\
+            int ret = store(S->memory, S->memory_bytes, addr, size, x);
+            if (ret != 0) {
+                S->bad = addr;
+                RAISE(ret);
+            }'''
+        ),
 
-    STORE = (0x9, ['x', 'addr', 'size'], [], Code('''\
-        int ret = store(S->memory, S->memory_bytes, addr, size, x);
-        if (ret != 0) {
-            S->bad = addr;
-            RAISE(ret);
-        }'''
-    ))
+        'LIT': Code('FETCH_PC(n);'),
 
-    LIT = (0xa, [], ['n'], Code('''\
-        FETCH_PC(n);'''
-    ))
+        'LIT_PC_REL': Code('''\
+            FETCH_PC(n);
+            n += S->pc - MIT_WORD_BYTES;'''
+        ),
 
-    LIT_PC_REL = (0xb, [], ['n'], Code('''\
-        FETCH_PC(n);
-        n += S->pc - MIT_WORD_BYTES;'''
-    ))
+        'LIT_0': Code('zero = 0;'),
+            'LIT_1': Code('one = 1;'),
+            'LIT_2': Code('two = 2;'),
+            'LIT_3': Code('three = 3;'),
 
-    LIT_0 = (0xc, [], ['zero'], Code('''\
-        zero = 0;'''
-    ))
+        'LT': Code('flag = a < b;'),
+            'ULT': Code('flag = (mit_uword)a < (mit_uword)b;'),
 
-    LIT_1 = (0xd, [], ['one'], Code('''\
-        one = 1;'''
-    ))
+        'NEGATE': Code('r = -a;'),
+            'ADD': Code('r = a + b;'),
+            'MUL': Code('r = a * b;'),
 
-    LIT_2 = (0xe, [], ['two'], Code('''\
-        two = 2;'''
-    ))
+        'DIVMOD': Code('''\
+            if (b == 0)
+              RAISE(MIT_ERROR_DIVISION_BY_ZERO);
+            q = a / b;
+            r = a % b;'''
+        ),
 
-    LIT_3 = (0xf, [], ['three'], Code('''\
-        three = 3;'''
-    ))
+        'UDIVMOD': Code('''\
+            if (b == 0)
+              RAISE(MIT_ERROR_DIVISION_BY_ZERO);
+            q = (mit_word)((mit_uword)a / (mit_uword)b);
+            r = (mit_word)((mit_uword)a % (mit_uword)b);'''
+        ),
 
-    LT = (0x11, ['a', 'b'], ['flag'], Code('''\
-        flag = a < b;'''
-    ))
+        'NOT': Code('r = ~x;'),
+            'AND': Code('r = x & y;'),
+            'OR': Code('r = x | y;'),
+            'XOR': Code('r = x ^ y;'),
 
-    ULT = (0x12, ['a', 'b'], ['flag'], Code('''\
-        flag = (mit_uword)a < (mit_uword)b;'''
-    ))
+        'LSHIFT': Code('r = n < (mit_word)MIT_WORD_BIT ? x << n : 0;'),
+            'RSHIFT': Code('''\
+            r = n < (mit_word)MIT_WORD_BIT ?
+                (mit_word)((mit_uword)x >> n) : 0;'''
+            ),
+            'ARSHIFT': Code('r = ARSHIFT(x, n);'),
 
-    NEGATE = (0x13, ['a'], ['r'], Code('''\
-        r = -a;'''
-    ))
+        'SIGN_EXTEND': Code('''\
+            n2 = n1 << (MIT_WORD_BYTES - (1 << size)) * MIT_BYTE_BIT;
+            n2 = ARSHIFT(n2, (MIT_WORD_BYTES - (1 << size)) * MIT_BYTE_BIT);'''
+        ),
+    },
+)
 
-    ADD = (0x14, ['a', 'b'], ['r'], Code('''\
-        r = a + b;'''
-    ))
-
-    MUL = (0x15, ['a', 'b'], ['r'], Code('''\
-        r = a * b;'''
-    ))
-
-    DIVMOD = (0x16, ['a', 'b'], ['q', 'r'], Code('''\
-        if (b == 0)
-          RAISE(MIT_ERROR_DIVISION_BY_ZERO);
-        q = a / b;
-        r = a % b;'''
-    ))
-
-    UDIVMOD = (0x17, ['a', 'b'], ['q', 'r'], Code('''\
-        if (b == 0)
-          RAISE(MIT_ERROR_DIVISION_BY_ZERO);
-        q = (mit_word)((mit_uword)a / (mit_uword)b);
-        r = (mit_word)((mit_uword)a % (mit_uword)b);'''
-    ))
-
-    NOT = (0x18, ['x'], ['r'], Code('''\
-        r = ~x;'''
-    ))
-
-    AND = (0x19, ['x', 'y'], ['r'], Code('''\
-        r = x & y;'''
-    ))
-
-    OR = (0x1a, ['x', 'y'], ['r'], Code('''\
-        r = x | y;'''
-    ))
-
-    XOR = (0x1b, ['x', 'y'], ['r'], Code('''\
-        r = x ^ y;'''
-    ))
-
-    LSHIFT = (0x1c, ['x', 'n'], ['r'], Code('''\
-        r = n < (mit_word)MIT_WORD_BIT ? x << n : 0;'''
-    ))
-
-    RSHIFT = (0x1d, ['x', 'n'], ['r'], Code('''\
-        r = n < (mit_word)MIT_WORD_BIT ? (mit_word)((mit_uword)x >> n) : 0;'''
-    ))
-
-    ARSHIFT = (0x1e, ['x', 'n'], ['r'], Code('''\
-        r = ARSHIFT(x, n);'''
-    ))
-
-    SIGN_EXTEND = (0x1f, ['n1', 'size'], ['n2'], Code('''\
-        n2 = n1 << (MIT_WORD_BYTES - (1 << size)) * MIT_BYTE_BIT;
-        n2 = ARSHIFT(n2, (MIT_WORD_BYTES - (1 << size)) * MIT_BYTE_BIT);'''
-    ))
-
-@unique
-class InternalExtraInstruction(InstructionEnum):
-    'Internal extra instruction opcodes.'
-    HALT = (0x1, [], [], Code('''\
-        RAISE(MIT_ERROR_HALT);'''
-    ))
+InternalExtraInstruction = instruction_enum(
+    'InternalExtraInstruction',
+    'Internal extra instruction opcodes.',
+    spec['InternalExtraInstruction'],
+    {
+        'HALT': Code('RAISE(MIT_ERROR_HALT);'),
+    },
+)
