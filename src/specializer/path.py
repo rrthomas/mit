7f4219ea12389cb91e61489de5389711d606e8f9
Reuben Thomas <rrt@sc3d.org>
2019-12-08 22:44:21 +0000

    specializer/path.py: fix a typo

--- src/specializer/path.py
+++ src/specializer/path.py
@@ -85,7 +85,7 @@ class State:
 
     def cached_depth(self):
         '''
-        Returns the number of stack items that are known to be chacheable.
+        Returns the number of stack items that are known to be cacheable.
         '''
         return self.stack_pos - self.stack_min
 
