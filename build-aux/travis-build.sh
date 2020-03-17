a0e23decf7013f321b423cf65ab71e9a12482c50
Reuben Thomas <rrt@sc3d.org>
2020-03-17 08:50:57 +0000

    Travis: move build script into separate file
    This is in preparation for containerized builds.
    
    Additionally, fix ASAN build, which has never worked on Travis.

---
+++ build-aux/travis-build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Build on Travis
+# (c) Mit authors 2020
+# This file is in the public domain.
+
+set -e
+
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then pip3 install pyyaml; fi
+
+./bootstrap
+if [[ "$ASAN" == "yes" ]]; then
+    CONFIGURE_ARGS=(--enable-package-suffix PYTHON=/usr/bin/python3.8 CFLAGS="-g3 -fsanitize=address -fsanitize=undefined" LDFLAGS="-fsanitize=address -fsanitize=undefined" PY_LOG_ENV="LD_PRELOAD=/usr/lib/gcc/x86_64-linux-gnu/7/libasan.so PYTHONMALLOC=malloc")
+fi
+./configure --enable-silent-rules "${CONFIGURE_ARGS[@]}" && make
+
+# Can't use libraries in-tree on macOS, so "make install", and don't "make
+# distcheck".
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then sudo make install; fi
+make check && if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then make distcheck; fi
