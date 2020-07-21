3b15d5d6047081e4fda1f733bb49785dabedf826
Alistair Turnbull <github@minworks.co.uk>
2020-07-21 16:13:51 +0100

    tests/extra.py: Overhaul

--- tests/extra.py
+++ tests/extra.py
@@ -15,28 +15,34 @@ from mit.globals import *
 from mit.binding import register_args
 
 
+def do_test():
+    '''
+    Runs the code from `VM.pc` to `assembler.pc`.
+    Reads the following globals:
+     - test - str - the name of the test
+     - final_callback - function - passed to `trace()`
+    '''
+    print(f"Test {test}", file=sys.stderr)
+    end = label()
+    trace(addr=end + word_bytes, final_callback=final_callback)
+    VM.pc = end
+
 # Data for ARGC/ARGV tests
 args = [b"foo", b"bard", b"basilisk"]
 register_args(*args)
 
-# Test code
-buffer = M.start + 0x200
-tests = [] # list of (start, end, final_callback)
-
-# Test ARGC
-start = label()
+test = "ARGC"
 extra(ARGC)
 
-def argc_test_callback(handler, stack):
+def final_callback(handler, stack):
     argc = stack.pop()
     handler.log(f"argc is {argc}, and should be {len(args)}")
     if argc != len(args):
-        handler.log(f"Error in ARGC test: pc = {handler.state.pc:#x}")
+        handler.log(f"Error in extra instructions and traps test {test}: pc = {handler.state.pc:#x}")
         sys.exit(1)
-tests.append((start, label(), argc_test_callback))
+do_test()
 
-# Test ARGV
-start = label()
+test = "ARGV and LibC.STRLEN"
 extra(ARGV)
 push(word_bytes)
 ass(ADD)
@@ -44,16 +50,16 @@ ass(LOAD)
 push(LibC.STRLEN)
 trap(LIBC)
 
-def argv_test_callback(handler, stack):
+def final_callback(handler, stack):
     arg1len = stack.pop()
     handler.log(f"arg 1's length is {arg1len}, and should be {len(args[1])}")
     if arg1len != len(args[1]):
-        handler.log(f"Error in extra instructions and trap tests: pc = {handler.state.pc:#x}")
+        handler.log(f"Error in extra instructions and traps test {test}: pc = {handler.state.pc:#x}")
         sys.exit(1)
-tests.append((start, label(), argv_test_callback))
+do_test()
 
-# Test LibC.STRNCPY
-start = label()
+test = "LibC.STRNCPY"
+buffer = M.start + 0x200
 push(buffer)
 extra(ARGV)
 push(word_bytes)
@@ -63,20 +69,14 @@ push(len(args[1]))
 push(LibC.STRNCPY)
 trap(LIBC)
 
-def strncpy_test_callback(handler, stack):
+def final_callback(handler, stack):
     handler.log(f"addr: {buffer:#x}")
     c_str = string_at(cast(buffer, c_char_p))
     handler.log(f"arg 1 is {c_str}, and should be {args[1]}")
     if c_str != args[1]:
-        handler.log(f"Error in extra instructions and trap tests: pc = {handler.state.pc:#x}")
+        handler.log(f"Error in extra instructions and traps test {test}: pc = {handler.state.pc:#x}")
         sys.exit(1)
-tests.append((start, label(), strncpy_test_callback))
+do_test()
 
-
-# Run tests
-for i, (start, end, final_callback) in enumerate(tests):
-    print(f"Test {i + 1}", file=sys.stderr)
-    VM.pc = start
-    trace(addr=end + word_bytes, final_callback=final_callback)
 
 print("Extra instructions and trap tests ran OK")
